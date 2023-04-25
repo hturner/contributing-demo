e26bd006159540bccb3b0e2e8c8aae18cfc59719
Heather Turner <ht@heatherturner.net>
2023-04-25 15:55:33 +0100

    rename pdfs

--- print_to_pdf.R
+++ print_to_pdf.R
@@ -1,6 +1,11 @@
 # actual
 library(renderthis)
-to_pdf("contributing-lightning.html")
+to_pdf("contributing-lightning.html",
+       to = "Turner_Heather_CW23_LightningTalk.pdf")
+
+
+to_pdf("index.html",
+       to = "Turner_Heather_CW23_Demo.pdf")
 
 # under the hood - allows more customisation if necessary
 pagedown::chrome_print(input = "contributing-lightning.html", 
